d08ef9d00b9ea77e1e1d5c117d3c7fc37524936c
ThiagoSoares <thiagocsoares@gmail.com>
2026-03-25 17:36:49 -0300

    chore: Rename files, remove duplicates

--- Exercicio02_Objeto/Principal.java
+++ Exercicio02_Objeto/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
         // A variável 'objetoAluno' vai guardar a referência para esse objeto.
         Aluno objetoAluno = new Aluno();
 
+        // System.out.println(objetoAluno);
         System.out.println("Exercicio 02 - Objeto criado com sucesso.");
         System.out.println("Esse objetoAluno é um Aluno real (instancia) criado a partir da classe Aluno.");
 
